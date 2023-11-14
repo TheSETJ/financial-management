e26c786c1d3aa34659de0f97bc4399ff1c031cdf
Sayyed Ehsan Taheri Javid <ehsantaheri25@yahoo.com>
2023-11-14 16:32:51 +0330

    Add validation: UpdateTransactionRequest

---
+++ app/Http/Requests/UpdateTransactionRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->id === $this->route('transaction')->user_id;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type' => ['in:paid,received,transferred'],
+            'amount' => ['integer', 'gt:0'],
+            'description' => ['string'],
+            'performed_at' => ['date_format:Y-m-d H:i:s']
+        ];
+    }
+}
